07c59d6b966a7732d548dc4310685edca48f0a1a
choi2601 <byeonghyeonchoe55@gmail.com>
2023-08-20 21:08:33 +0900

    API를 통한 사용자 구성 요소

--- Ch04/Web-Component/main.js
+++ Ch04/Web-Component/main.js
@@ -1,6 +1,8 @@
 import HelloWorld from "./components/HelloWorld";
+import GitHubAvatar from "./components/GitHubAvatar";
 
 window.customElements.define("hello-world", HelloWorld);
+window.customElements.define("github-avatar", GitHubAvatar);
 
 /**
  * @description HelloWorld 구성 요소의 color 속성을 변경할 이벤트 바인딩
