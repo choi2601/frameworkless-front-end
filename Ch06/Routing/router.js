db1e7a620b28e6e40c516324940672d8b8099861
choi2601 <byeonghyeonchoe55@gmail.com>
2023-08-26 13:20:32 +0900

    경로 매개변수 값 추출를 위한 정규표현 헬퍼

--- Ch06/Routing/router.js
+++ Ch06/Routing/router.js
@@ -1,3 +1,25 @@
+const ROUTE_PARAMETER_REGEXP = /:(\w+)/g;
+const URL_FRAGMENT_REGEXP = "([^\\/]+)";
+
+const extractUrlParams = (route, windowHash) => {
+  const params = {};
+
+  if (route.params.length === 0) {
+    return params;
+  }
+
+  const matches = windowHash.match(route.testRegExp);
+
+  matches.shift();
+
+  matches.forEach((paramValue, index) => {
+    const paramName = route.params[index];
+    params[paramName] = paramValue;
+  });
+
+  return params;
+};
+
 export default () => {
   const routes = [];
 
@@ -6,8 +28,11 @@ export default () => {
   const router = {};
 
   const checkRoutes = () => {
+    const { hash } = window.location;
+
     const currentRoute = routes.find((route) => {
-      return route.fragment === window.location.hash;
+      const { testRegExp } = route;
+      return testRegExp.test(hash);
     });
 
     if (!currentRoute) {
@@ -15,11 +40,26 @@ export default () => {
       return;
     }
 
-    currentRoute.component();
+    const urlParams = extractUrlParams(currentRoute, window.location.hash);
+
+    currentRoute.component(urlParams);
   };
 
   router.addRoute = (fragment, component) => {
-    routes.push({ fragment, component });
+    const params = [];
+
+    const parsedFragment = fragment
+      .replace(ROUTE_PARAMETER_REGEXP, (match, paramName) => {
+        params.push(paramName);
+        return URL_FRAGMENT_REGEXP;
+      })
+      .replace(/\//g, "\\/");
+
+    routes.push({
+      textRegExp: new RegExp(`^${parsedFragment}$`),
+      component,
+      params,
+    });
 
     return router;
   };
