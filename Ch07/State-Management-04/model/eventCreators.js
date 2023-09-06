7a8f04304f9f6fcf9803d9376a439cd62ef6f0ff
choi2601 <byeonghyeonchoe55@gmail.com>
2023-09-06 21:46:09 +0900

    event 객체 생성 헬퍼 함수 추가

---
+++ Ch07/State-Management-04/model/eventCreators.js
@@ -0,0 +1,45 @@
+/**
+ * @description 발생한 상황을 식별하는 타입과 이벤트 처리를 위한 정보인 payload를 랩핑
+ */
+
+const EVENT_TYPES = Object.freeze({
+  ITEM_ADDED: "ITEM_ADDED",
+  ITEM_UPDATED: "ITEM_UPDATED",
+  ITEM_DELETED: "ITEM_DELETED",
+  ITEMS_COMPLETED_TOGGLED: "ITEMS_COMPLETED_TOGGLED",
+  ITEMS_MARKED_AS_COMPLETED: "ITEMS_MARKED_AS_COMPLETED",
+  COMPLETED_ITEM_DELETED: "COMPLETED_ITEM_DELETED",
+  FILTER_CHANGED: "FILTER_CHANGED",
+});
+
+export default {
+  addItem: (text) => ({
+    type: EVENT_TYPES.ITEM_ADDED,
+    payload: text,
+  }),
+  updateItem: (index, text) => ({
+    type: EVENT_TYPES.ITEM_UPDATED,
+    payload: {
+      text,
+      index,
+    },
+  }),
+  deleteItem: (index) => ({
+    type: EVENT_TYPES.ITEM_DELETED,
+    payload: index,
+  }),
+  toggleItemCompleted: (index) => ({
+    type: EVENT_TYPES.ITEMS_COMPLETED_TOGGLED,
+    payload: index,
+  }),
+  completeAll: () => ({
+    type: EVENT_TYPES.ITEMS_MARKED_AS_COMPLETED,
+  }),
+  clearCompleted: () => ({
+    type: EVENT_TYPES.COMPLETED_ITEM_DELETED,
+  }),
+  changeFilter: (filter) => ({
+    type: EVENT_TYPES.FILTER_CHANGED,
+    payload: filter,
+  }),
+};
